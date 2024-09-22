78eba83aa841b5afb99da0d9f90a90f759909264
Priscilla Melo <priscillamelo73@gmail.com>
2024-09-22 16:16:51 -0300

    feat: tabs no data

---
+++ lib/interface/widget/completed_tab.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CompletedTab extends StatelessWidget {
+  const CompletedTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: Text("Não Há Testes Finalizados!"));
+  }
+}
---
+++ lib/interface/widget/progress_tab.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_seedbyseed/interface/widget/component/floating_button_component.dart';
+
+class ProgressTab extends StatelessWidget {
+  const ProgressTab({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      floatingActionButton: FloatingButtonSmallComponent(),
+    );
+
+    /* Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            FloatingButtonLargeComponent(),
+            
+            SizedBox(
+              height: 16,
+            ),
+            Text("Iniciar Teste de Germinação"),
+          ],
+        ),
+      ); */
+  }
+}
