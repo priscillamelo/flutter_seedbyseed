1358f20cd936a9aacfc977d1ca64b5fe10846e84
Priscilla Melo <priscillamelo73@gmail.com>
2024-09-22 16:15:17 -0300

    feat: floating button (normal and small) component

---
+++ lib/interface/widget/component/floating_button_component.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_seedbyseed/route/routes.dart';
+
+class FloatingButtonLargeComponent extends StatelessWidget {
+  const FloatingButtonLargeComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.large(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+      onPressed: () =>
+          Navigator.pushNamed(context, PageRoutes.kADD_GERMINATIONTEST),
+      child: const Icon(
+        Icons.add,
+        size: 64,
+      ),
+    );
+  }
+}
+
+class FloatingButtonSmallComponent extends StatelessWidget {
+  const FloatingButtonSmallComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton(
+      tooltip: "Iniciar Teste de Germinação",
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(50)),
+      onPressed: () {
+        Navigator.pushNamed(context, PageRoutes.kADD_GERMINATIONTEST);
+      },
+      child: const Icon(
+        Icons.add,
+        size: 36,
+      ),
+    );
+  }
+}
