c05e9016fd9763b3d727530de894a448ed110c14
Priscilla Melo <priscillamelo73@gmail.com>
2024-10-03 17:34:38 -0300

    feat: create routes

--- lib/route/routes.dart
+++ lib/route/routes.dart
@@ -1,11 +1,12 @@
-
 // ignore_for_file: constant_identifier_names
 
 class PageRoutes {
   static const String kHOME = "/home";
   static const String kADD_GERMINATIONTEST = "/add-germination-test";
+  static const String kADD_REPETITIONGERMINATIONTEST =
+      "/add-repetition-germination-test";
   static const String kUPDATE_GERMINATIONTEST = "/update-germination-test";
-  
+
   //static const String kLOGIN_PAGE = "/login";
   //static const String kCREATE_ACCOUNT = "/create-account";
   //static const String kINFO_DATA_PAGE = "/information_data";
